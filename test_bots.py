97ded446f6b44b9b4cceefadece2066c3800bc7f
Danya Generalov <danya02@me.com>
2018-12-18 08:25:19 +0300

    Adding a chat greeter bot.

--- test_bots.py
+++ test_bots.py
@@ -27,8 +27,27 @@ class EchoBot(bot.VKBot):
     def on_message(self,message,session):
         self.send_message(message='Received message: '+str(message))
 
+class GreeterBot(bot.VKBot):
+    def __init__(self,greet_msg='Welcome, {name} ({screen_name})!', goodbye_msg='Goodbye, {name} ({screen_name}).'):
+        self.greet_message=greet_msg
+        self.goodbye_message=goodbye_msg
+    def on_message(self, message, session):
+        if 'action' in message:
+            if message['action']['type']=='chat_invite_user':
+                member_id = str(message['action']['member_id'])
+                data=session.vk_session.get_api().users.get(user_ids=member_id, fields='screen_name')[0]
+                name=data['first_name']+' '+data['last_name']
+                screen_name=data['screen_name']
+                self.send_message(message=self.greet_message.format(name=name, screen_name=screen_name))
+            elif message['action']['type']=='chat_kick_user':
+                member_id = str(message['action']['member_id'])
+                data=session.vk_session.get_api().users.get(user_ids=member_id, fields='screen_name')[0]
+                name=data['first_name']+' '+data['last_name']
+                screen_name=data['screen_name']
+                self.send_message(message=self.goodbye_message.format(name=name, screen_name=screen_name))
+
 if __name__ == '__main__':
-    k=EchoBot()
+    k=GreeterBot()
     s=vk_api.VkApi(login=input('Login: '), password=getpass.getpass())
     s.auth()
     supers=main.Session(s, 2000000001)
